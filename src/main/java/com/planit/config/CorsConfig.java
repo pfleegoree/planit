c469cd8f58530173c9672a7f65f8f84b69c56e94
Elena Pfleegor <pfleegoree@gmail.com>
2026-01-11 22:35:35 -0600

    well oopsy .allowedOriginPatterns was correct for the wildcard, duh

--- src/main/java/com/planit/config/CorsConfig.java
+++ src/main/java/com/planit/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins(
+                        .allowedOriginPatterns(
                                 "https://*.netlify.app",
                                 "http://localhost:3000"                  // optional local dev
                         )
